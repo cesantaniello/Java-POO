8360735e9cad126ac8bb37ea1c5fd749e9840d14
Carlos <carlos.e.santaniello@gmail.com>
2020-10-27 20:05:14 +0100

    1.5 Final: Constantes

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         Doctor myDoctor = new Doctor("Anahí Salgado");
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
 
         Doctor myDoctorAnn = new Doctor();
         myDoctor.showId();
-
+*/
         showMenu();
     }
 
