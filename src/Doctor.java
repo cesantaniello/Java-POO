0a884110fb9956f50d9f409bc7e43765a4c6e7a5
Carlos <carlos.e.santaniello@gmail.com>
2020-10-27 19:36:31 +0100

    1.4 Creando elementos estáticos

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,13 +3,14 @@
  */
 public class Doctor {
     //Propiedades
-    int id;
+    static int id = 0; //Autoincremental
     String name;
     String speciality;
 
     //Método constructor
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
     Doctor(String name){
         System.out.println("El nombre del Doctor asignado es: " + name);
@@ -17,5 +18,9 @@ public class Doctor {
     //Métodos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Carlos Pérez");
-        myDoctor.name = "Alejadro Gómez";
+
+        Doctor myDoctor = new Doctor("Anahí Salgado");
+        myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        showMenu();
     }
+
 }
