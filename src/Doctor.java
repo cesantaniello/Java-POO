ef943a049e2d0fbd6697c9bf16b4af542249eade
Carlos <carlos.e.santaniello@gmail.com>
2020-10-27 21:59:29 +0100

    1.6 Variable Vs. Objeto

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,21 +1,23 @@
-/**
- * Clase pública Doctor
- */
+
 public class Doctor {
-    //Propiedades
-    static int id = 0; //Autoincremental
+    //Atributos
+    static int id = 0; //Autoincrement
     String name;
+    String email;
     String speciality;
 
-    //Método constructor
     Doctor(){
-        System.out.println("Construyendo el objeto Doctor");
-        id++;
+        System.out.println("Construyendo el Objeto Doctor");
     }
-    Doctor(String name){
+
+    Doctor(String name, String speciality){
         System.out.println("El nombre del Doctor asignado es: " + name);
+        id++;
+        this.name = name;
+        this.speciality = speciality;
     }
-    //Métodos
+
+    //Comportamientos
     public void showName(){
         System.out.println(name);
     }
@@ -23,4 +25,5 @@ public class Doctor {
     public void showId(){
         System.out.println("ID Doctor: " + id);
     }
+
 }
