036276c7cf32b6acb8583585cf1aed90586ce30d
Carlos <carlos.e.santaniello@gmail.com>
2020-10-29 12:05:04 +0100

    2.1 Super y This

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,31 +1,22 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
+public class Doctor extends User{
     //Atributos
-    static int id = 0; //Autoincrement
-    private String name;
-    private String email;
     private String speciality;
 
-    Doctor(){
-        System.out.println("Construyendo el Objeto Doctor");
-    }
-
-    Doctor(String name, String speciality){
+    Doctor(String name, String email){
+        super(name, email);
         System.out.println("El nombre del Doctor asignado es: " + name);
-        id++;
-        this.name = name;
         this.speciality = speciality;
     }
 
-    //Comportamientos
-    public void showName(){
-        System.out.println(name);
+    public String getSpeciality() {
+        return speciality;
     }
 
-    public void showId(){
-        System.out.println("ID Doctor: " + id);
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
     }
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
